184f767cfe91beebcfe8525e821a5c26a102ba21
Andrian Dwi Haryanto <andriandwiharyanto.id@gmail.com>
2024-01-22 13:25:29 +0700

    update

---
+++ app/ui/fonts.ts
@@ -0,0 +1,15 @@
+import { Inter, Lobster, Lusitana } from "next/font/google";
+
+export const lobster = Lobster({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+export const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
